d3bde352d253811ef84fdb128658a2641b90477f
sukush99 <sukushkunwar99@gmail.com>
2025-03-11 16:18:18 +1100

    Initial boilerplate code: Completed. Dockerfile not accessing the settings file

--- page-rank/run.py
+++ page-rank/run.py
@@ -1,5 +1,9 @@
+from config import config
+
 def main():
-    print("Hello from page-rank!")
+    Link = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
+    print(f"Link: {Link}")
+    print(f"Link from config: {config.link}")
 
 
 if __name__ == "__main__":
